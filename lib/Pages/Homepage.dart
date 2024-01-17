4102115c96d2cd85729c566b32b66afa69c640a1
kevin Iradukunda <iradukundakvn8@gmail.com>
2024-01-17 12:59:48 +0200

    i just added the like section

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:instagram_uiclone/util/stories.dart';
 import 'package:instagram_uiclone/util/userposts.dart';
@@ -12,88 +13,90 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   final List people = [
-    "kevin",
+    "Your Story",
+    "Bruce Melodie",
     "theCatBabalao",
     "kelly",
     "clement",
-    "b_bex",
+    "naomie_m",
+    "Bryan",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          width: 900,
-          decoration: BoxDecoration(color: Colors.white),
-          child: Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Instagram",
-                style: GoogleFonts.rochester(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 33,
-                ),
+        title: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Instagram",
+              style: GoogleFonts.rochester(
+                fontWeight: FontWeight.bold,
+                fontSize: 33,
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12.0),
-                child: Icon(
-                  Icons.arrow_drop_down,
-                  size: 28,
-                ),
+            ),
+            const Padding(
+              padding: const EdgeInsets.only(top: 12.0, left: 7.0),
+              child: Icon(
+                FontAwesomeIcons.chevronDown,
+                size: 16,
+                color: Colors.black,
               ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(top: 7.0),
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.favorite_outline,
-                        size: 28.0,
-                      ),
+            ),
+            // const Spacer(),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(top: 7.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.favorite_outline,
+                      size: 28.0,
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.send,
-                        size: 28.0,
-                      ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      const FaIcon(FontAwesomeIcons.facebookMessenger).icon,
+                      size: 25.0,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 5,
-          ),
-          Container(
-            height: 130,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: people.length,
-              itemBuilder: (context, index) {
-                return Stories(
-                  name: people[index],
-                );
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5,
             ),
-          ),
-          const UserPost(
-            names: "kelly",
-          ),
-        ],
+            Container(
+              height: 130,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: people.length,
+                itemBuilder: (context, index) {
+                  return Stories(
+                    name: people[index],
+                  );
+                },
+              ),
+            ),
+            const UserPost(
+              names: "kelly",
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        items: [
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
--- lib/util/userposts.dart
+++ lib/util/userposts.dart
@@ -54,28 +54,175 @@ class UserPost extends StatelessWidget {
                   fit: BoxFit.cover)),
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.favorite_outline),
-              iconSize: 35,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.favorite_outline),
+                  iconSize: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(FontAwesomeIcons.comment),
+                    iconSize: 26,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5.0),
+                  child: Transform.rotate(
+                    angle: -100,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(FontAwesomeIcons.solidPaperPlane),
+                      iconSize: 26,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(FontAwesomeIcons.comment),
-              iconSize: 28,
+            const SizedBox(
+              width: 190,
             ),
-            Transform.rotate(
-              angle: -100,
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(FontAwesomeIcons.solidPaperPlane),
-                iconSize: 30,
-              ),
+            Row(
+              children: [
+                IconButton(
+                  iconSize: 30,
+                  onPressed: () {},
+                  icon: Icon(Icons.bookmark_border),
+                ),
+              ],
             ),
           ],
         ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: 9.0,
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 30.0,
+                height: 20.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/Ishema.jpg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+             const Text("Liked by"),
+              const SizedBox(
+                width: 5.0,
+              ),
+             const  Text(
+                "africa__250k",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        //this is the seconnd post on instagram
+        //   Padding(
+        //     padding: const EdgeInsets.all(16.0),
+        //     child: Row(
+        //       children: [
+        //         Container(
+        //           width: 40,
+        //           height: 40,
+        //           decoration: BoxDecoration(
+        //             image: const DecorationImage(
+        //               image: AssetImage('assets/images/Ishema.jpg'),
+        //               fit: BoxFit.cover,
+        //             ),
+        //             shape: BoxShape.circle,
+        //             color: Colors.grey[400],
+        //           ),
+        //         ),
+        //         SizedBox(
+        //           width: 10.0,
+        //         ),
+        //         Text(
+        //           names,
+        //           style: TextStyle(fontWeight: FontWeight.bold),
+        //         ),
+        //         // SizedBox(
+        //         //   width: 250.0,
+        //         // ),
+        //         Spacer(),
+        //         Row(
+        //           children: [
+        //             IconButton(
+        //               onPressed: () {},
+        //               icon: Icon(Icons.more_horiz_rounded),
+        //             ),
+        //           ],
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        //   Container(
+        //     height: 400,
+        //     decoration: const BoxDecoration(
+        //         image: DecorationImage(
+        //             image: AssetImage('assets/images/Isange.jpg'),
+        //             fit: BoxFit.cover)),
+        //   ),
+        //   Row(
+        //     children: [
+        //       Row(
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: [
+        //           IconButton(
+        //             onPressed: () {},
+        //             icon: const Icon(Icons.favorite_outline),
+        //             iconSize: 30,
+        //           ),
+        //           Padding(
+        //             padding: const EdgeInsets.only(bottom: 1.0),
+        //             child: IconButton(
+        //               onPressed: () {},
+        //               icon: const Icon(FontAwesomeIcons.comment),
+        //               iconSize: 26,
+        //             ),
+        //           ),
+        //           Padding(
+        //             padding: const EdgeInsets.only(bottom: 5.0),
+        //             child: Transform.rotate(
+        //               angle: -100,
+        //               child: IconButton(
+        //                 onPressed: () {},
+        //                 icon: const Icon(FontAwesomeIcons.solidPaperPlane),
+        //                 iconSize: 26,
+        //               ),
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //       const SizedBox(
+        //         width: 190,
+        //       ),
+        //       Row(
+        //         children: [
+        //           IconButton(
+        //             iconSize: 30,
+        //             onPressed: () {},
+        //             icon: Icon(Icons.bookmark_border),
+        //           ),
+        //         ],
+        //       ),
+        //     ],
+        //   ),
+        // ],
       ],
     );
   }
